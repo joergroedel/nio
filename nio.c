d87a0d54348bac05e082cd77af53ad7d429433ea
Joerg Roedel <jroedel@suse.de>
2015-08-20 15:31:32 +0200

    nio: Add socket setup code
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- nio.c
+++ nio.c
@@ -1,9 +1,59 @@
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
 #include <stdio.h>
 
+#define DEFAULT_PORT	"7124"
+
 static int threads = 1;
+
+int create_socket(int af, const char *hostname, const char *service)
+{
+	struct addrinfo hints, *results, *rp;
+	int err, fd = -1;
+
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family   = af;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags    = AI_PASSIVE | AI_NUMERICSERV;
+
+	err = getaddrinfo(hostname, service, &hints, &results);
+	if (err) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+		return -1;
+	}
+
+	for (rp = results; rp != NULL; rp = rp->ai_next) {
+		fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (fd == -1)
+			continue;
+
+		if (hostname == NULL) {
+			if (bind(fd, rp->ai_addr, rp->ai_addrlen) == 0)
+				break;
+		} else {
+			if (connect(fd, rp->ai_addr, rp->ai_addrlen) != -1)
+				break;
+		}
+
+		close(fd);
+		fd = -1;
+	}
+
+	if (rp == NULL) {
+		fprintf(stderr, "Could not resolve host %s\n", hostname);
+		return -1;
+	}
+
+	freeaddrinfo(results);
+
+	return fd;
+}
 
 void usage(const char *prg)
 {
@@ -12,21 +62,24 @@ void usage(const char *prg)
 	printf("    -c server     Client Mode - Send packets to server\n");
 	printf("    -p port       UDP port to bind to\n");
 	printf("    -t threads    Number of thread to start for sending/receiving\n");
-	printf("    -4            Force use of IPv4 (default is IPv6)\n");
+	printf("    -4            Force use of IPv4\n");
+	printf("    -6            Force use of IPv6\n");
 	printf("    -h            Print this help message and exit\n");
 }
 
 int main(int argc, char **argv)
 {
+	const char *service = DEFAULT_PORT;
+	int port = atoi(DEFAULT_PORT);
 	const char *hostname = NULL;
-	int domain = AF_INET6;
+	int domain = AF_UNSPEC;
 	int server = 0;
-	int port = -1;
+	int ctrl_fd;
 	int opt;
 
 	/* Parse options */
 	while (1) {
-		opt = getopt(argc, argv, "sr:p:t:h4");
+		opt = getopt(argc, argv, "sr:p:t:h46");
 		if (opt == EOF)
 			break;
 
@@ -38,13 +91,18 @@ int main(int argc, char **argv)
 			hostname = optarg;
 			break;
 		case 'p':
-			port = atoi(optarg);
+			service = optarg;
+			port = atoi(service);
 			break;
 		case 't':
 			threads = atoi(optarg);
 			break;
 		case '4':
 			domain = AF_INET;
+			break;
+		case '6':
+			domain = AF_INET6;
+			break;
 		default:
 			fprintf(stderr, "ERROR: Unknown option: %c\n", opt);
 			/* fall-through */
@@ -72,6 +130,12 @@ int main(int argc, char **argv)
 		usage(argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
+	ctrl_fd = create_socket(domain, hostname, service);
+	if (ctrl_fd == -1)
+		return EXIT_FAILURE;
+
+	close(ctrl_fd);
 
 	return 0;
 }
