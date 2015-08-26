670236d6fbf69eacc357762f134975938ac6d6d2
Joerg Roedel <jroedel@suse.de>
2015-08-26 11:31:35 +0200

    nio: Add client and server control loops
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- nio.c
+++ nio.c
@@ -1,5 +1,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
@@ -8,10 +9,11 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define DEFAULT_PORT	"7124"
 
-static int should_stop;
+static volatile int should_stop;
 static int threads = 1;
 
 static void sig_handler(int signum)
@@ -73,6 +75,199 @@ int create_socket(int af, const char *hostname, const char *service)
 	freeaddrinfo(results);
 
 	return fd;
+}
+
+#define CMD_START	1
+#define CMD_ACK		2
+#define CMD_STOP	3
+#define CMD_DATA	4
+
+struct nio_cmd {
+	uint32_t cmd;
+	uint32_t threads;
+	uint32_t seq_lo;
+	uint32_t seq_hi;
+	uint32_t recv_lo;
+	uint32_t recv_hi;
+} __attribute__((packed));
+
+enum states {
+	STATE_START,
+	STATE_START_SENT,
+	STATE_STARTED,
+	STATE_DYING,
+};
+
+void ctrl_server(int fd)
+{
+	enum states state = STATE_START;
+	struct nio_cmd cmd, recv_cmd;
+	struct sockaddr remote;
+	struct timeval tv;
+	fd_set rfds, wfds;
+	int cmd_write = 0;
+	socklen_t r_len;
+
+	while (state != STATE_DYING) {
+		int ret;
+
+		tv.tv_sec  = 1;
+		tv.tv_usec = 0;
+
+		FD_ZERO(&rfds);
+		FD_ZERO(&wfds);
+
+		FD_SET(fd, &rfds);
+		if (cmd_write)
+			FD_SET(fd, &wfds);
+
+		ret = select(fd + 1, &rfds, &wfds, NULL, &tv);
+		if (ret == -1) {
+			if (errno == EINTR)
+				continue;
+
+			perror("select");
+			exit(EXIT_FAILURE);
+		}
+
+		if (should_stop)
+			break;
+
+		if (FD_ISSET(fd, &wfds)) {
+			int sent;
+
+			if (state == STATE_START) {
+				sent = sendto(fd, &cmd, sizeof(cmd), 0,
+					      &remote, r_len);
+				if (sent != sizeof(cmd)) {
+					perror("sendto");
+					exit(EXIT_FAILURE);
+				}
+				cmd_write = 0;
+				state = STATE_STARTED;
+
+				printf("Server started\n");
+			}
+
+		}
+
+		if (FD_ISSET(fd, &rfds)) {
+			ssize_t bytes;
+
+			r_len = sizeof(remote);
+			bytes = recvfrom(fd, &recv_cmd, sizeof(recv_cmd), 0,
+					 &remote, &r_len);
+			if (bytes != sizeof(recv_cmd))
+				continue;
+
+			switch (ntohl(recv_cmd.cmd)) {
+			case CMD_START:
+				if (state != STATE_START)
+					break;
+
+				memset(&cmd, 0, sizeof(cmd));
+				cmd.cmd   = htonl(CMD_ACK);
+				cmd_write = 1;
+
+				break;
+			case CMD_STOP:
+				state = STATE_DYING;
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+void ctrl_client(int fd)
+{
+	enum states state = STATE_START;
+	struct nio_cmd cmd, recv_cmd;
+	struct timeval tv;
+	fd_set rfds, wfds;
+	int cmd_write = 0;
+
+	memset(&cmd, 0, sizeof(cmd));
+	cmd.cmd     = htonl(CMD_START);
+	cmd.threads = htonl(threads);
+	cmd_write = 1;
+
+	while (state != STATE_DYING) {
+		int ret;
+
+		tv.tv_sec  = 1;
+		tv.tv_usec = 0;
+
+		FD_ZERO(&rfds);
+		FD_ZERO(&wfds);
+
+		FD_SET(fd, &rfds);
+		if (cmd_write)
+			FD_SET(fd, &wfds);
+
+		if (should_stop) {
+			memset(&cmd, 0, sizeof(cmd));
+			cmd.cmd   = htonl(CMD_STOP);
+			cmd_write = 1;
+		}
+
+		ret = select(fd + 1, &rfds, &wfds, NULL, &tv);
+		if (ret == -1) {
+			if (errno == EINTR)
+				continue;
+
+			perror("select");
+			exit(EXIT_FAILURE);
+		}
+
+		if (FD_ISSET(fd, &wfds)) {
+			ssize_t sent;
+
+			sent = send(fd, &cmd, sizeof(cmd), 0);
+			if (sent != sizeof(cmd)) {
+				perror("send");
+				exit(EXIT_FAILURE);
+			}
+
+			if (state == STATE_START)
+				state = STATE_START_SENT;
+
+			if (should_stop)
+				state = STATE_DYING;
+
+			cmd_write = 0;
+		}
+
+		if (FD_ISSET(fd, &rfds)) {
+			ssize_t bytes;
+			uint32_t cmd;
+
+			bytes = recv(fd, &recv_cmd, sizeof(recv_cmd), 0);
+			if (bytes != sizeof(recv_cmd)) {
+				perror("recv");
+				continue;
+			}
+
+			cmd = ntohl(recv_cmd.cmd);
+
+			switch (cmd) {
+			case CMD_ACK:
+				if (state == STATE_START_SENT) {
+					printf("Client started\n");
+					state = STATE_STARTED;
+				}
+				break;
+			case CMD_DATA:
+				if (state != STATE_STARTED)
+					break;
+				/* Handle data */
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
 
 void usage(const char *prg)
@@ -161,6 +356,11 @@ int main(int argc, char **argv)
 	ctrl_fd = create_socket(domain, hostname, service);
 	if (ctrl_fd == -1)
 		return EXIT_FAILURE;
+
+	if (server)
+		ctrl_server(ctrl_fd);
+	else
+		ctrl_client(ctrl_fd);
 
 	close(ctrl_fd);
 
